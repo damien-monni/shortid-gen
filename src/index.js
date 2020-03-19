a48c81df60eaa5e707dcfdcc0ed3e76902b9d5e9
Damien Monni <contact@damien-monni.com>
2020-03-19 12:05:33 +0100

    Initial commit with a simple static site

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import shortid from 'shortid';
+
+const inputEl = document.getElementById('generated-input');
+
+const onGenerate = (event) => {
+  event && event.preventDefault();
+
+  const id = shortid.generate();
+  inputEl.value = id;
+};
+
+const generateButtonEl = document.getElementById('generate-button');
+generateButtonEl.addEventListener('click', onGenerate);
+
+onGenerate();
